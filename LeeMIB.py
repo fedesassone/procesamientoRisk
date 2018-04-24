fb32cd377a813b9d895dae1d2f736a538f9701d6
loller67 <ollerr67@gmail.com>
2018-04-24 19:39:07 -0300

    el screeeeeeeeept

--- LeeMIB.py
+++ LeeMIB.py
@@ -5,18 +5,18 @@ lineas = fh.readlines() # lee todo el archivo
 nlines = len(lineas)
 
 fw = open('10068.MIB.csv','w')
-fw.writelines('total time,time,RR,beat')
+fw.writelines('time,total RR,RR,beat')
 fw.write('\n')
 tiempo = 0.
-tiempoTotal = 0.
+totalRR = 0
 for j in range(5,nlines-1):
     line = lineas[j]
     w0 = line[0:1]
     w1 = line[1:-1]
-    print(w0, w1, tiempo, tiempoTotal)
+    totalRR = int(float(w1)) + totalRR
+    print(w0, totalRR, w1, tiempo)
     tiempo = tiempo + float(w1)/1000.
-    tiempoTotal = tiempo + tiempoTotal
-    wline = str(tiempoTotal) + ',' + str(tiempo) + ',' + w1 + ',' + w0 
+    wline = str(tiempo) + ',' + str(totalRR) + ',' + w1 + ',' + w0 
     fw.write(wline)
     fw.write('\n')
 

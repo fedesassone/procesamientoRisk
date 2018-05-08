a256928a52e01bceece08832120f502eb6a8d666
Luca Oller <luca.oller@avature.net>
2018-05-08 20:51:26 -0300

    agregue cosas

--- TP.R
+++ TP.R
@@ -37,3 +37,26 @@ op <- par(mfrow = c(1, 1))
 plot(Frecuencia, fft.RRlineal , type='l' , xlim=c(0,0.2), ylim=c(0,30000),col='red')
 lines (Frecuencia, fft.RRspline, col='green')
 
+#muestras perdidas
+
+dir = ''
+dato.rr = read.csv(paste( dir , '10068Filtrado.csv',sep=''))
+print(names(dato.rr))
+# "t" "RR"
+plot(dato.rr $t,dato.rr $RR,type='l', ylab='RR (ms)',xlab='tiempo (s)')
+points(dato.rr $t,dato.rr $RR,pch=20)
+
+
+library (muStat)
+N = 48
+tiempo = 0:(N-1)
+st1 = rnorm(N,mean=1,sd=0.5)
+indices = runif(25,min=5,max=45)
+st1[ indices ] = NA
+st11 = st1[ is.number(st1)]
+tiempo1 = tiempo[is.number(st1)]
+plot(tiempo1,st11, type='l')
+st2 = spline(tiempo1,st11,N)
+plot(tiempo,st1, type='b')
+lines (st2$x, st2$y, col='red', lty =2)
+points(st2$x, st2$y,pch=20,col='red')

72785fbc4849effc500d9376d92de1048f5da4d3
Luca Oller <luca.oller@avature.net>
2018-05-15 20:47:12 -0300

    esta fallando la parte de muestras perdidas

--- TP.R
+++ TP.R
@@ -9,14 +9,8 @@ points(dato.rr $t,dato.rr $RR,pch=20)
 
 #parte 2
 
-plot(dato.rr$t,dato.rr$RR,type='l', ylab='RR (ms)',xlab='tiempo
-     (s)', xlim=c(237,247),ylim=c(600,1200))
+plot(dato.rr$t,dato.rr$RR,type='l', ylab='RR (ms)',xlab='tiempo(s)', xlim=c(0,72473),ylim=c(0,8000))
 points(dato.rr$t,dato.rr$RR,pch=20)
-N = length(dato.rr$t)
-for(j in 1:N)
-{
-  abline (v=dato.rr$t[ j ], lty =2)
-}
 Nmuestro = as.integer(2*max(dato.rr$t))
 RRlineal = approx(dato.rr$t,dato.rr$RR,n=Nmuestro)
 RRspline = spline(dato.rr$t,dato.rr$RR,n=Nmuestro)
@@ -24,18 +18,6 @@ lines (RRlineal$x, RRlineal$y, col='red')
 points(RRlineal$x, RRlineal$y,pch=20,col='red')
 lines (RRspline$x,RRspline$y, col='blue')
 points(RRspline$x,RRspline$y,pch=20,col='green')
-
-#parte 3
-
-fft.RRlineal = Mod(fft(RRlineal$y))
-fft.RRspline = Mod(fft(RRspline$y))
-tiempo = 0:(length( fft.RRlineal )-1)
-FrecuenciaMuestreo = 2
-DeltaFrecMuestreo = FrecuenciaMuestreo/N
-Frecuencia = DeltaFrecMuestreo*tiempo
-op <- par(mfrow = c(1, 1))
-plot(Frecuencia, fft.RRlineal , type='l' , xlim=c(0,0.2), ylim=c(0,30000),col='red')
-lines (Frecuencia, fft.RRspline, col='green')
 
 #muestras perdidas
 
@@ -48,7 +30,7 @@ points(dato.rr $t,dato.rr $RR,pch=20)
 
 
 library (muStat)
-N = 48
+N = 137857
 tiempo = 0:(N-1)
 st1 = rnorm(N,mean=1,sd=0.5)
 indices = runif(25,min=5,max=45)
@@ -60,3 +42,4 @@ st2 = spline(tiempo1,st11,N)
 plot(tiempo,st1, type='b')
 lines (st2$x, st2$y, col='red', lty =2)
 points(st2$x, st2$y,pch=20,col='red')
+

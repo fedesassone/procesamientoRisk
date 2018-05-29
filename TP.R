b76f65be39c3fbdb49809af9914f350290d0e62b
Luca Oller <luca.oller@avature.net>
2018-05-29 20:32:06 -0300

    lalala

--- TP.R
+++ TP.R
@@ -8,12 +8,14 @@ plot(dato.rr $t,dato.rr $RR,type='l', ylab='RR (ms)',xlab='tiempo (s)')
 points(dato.rr $t,dato.rr $RR,pch=20)
 
 #parte 2
-
-plot(dato.rr$t,dato.rr$RR,type='l', ylab='RR (ms)',xlab='tiempo(s)', xlim=c(0,72473),ylim=c(0,20000))
+tiempo_total = 71377.58399999907
+constante_min_heartbeat = 0.25
+delta_t = as.integer(tiempo_total/constante_min_heartbeat)
+plot(dato.rr$t,dato.rr$RR,type='l', ylab='RR (ms)',xlab='tiempo(s)', xlim=c(0,72473),ylim=c(0,8000))
 points(dato.rr$t,dato.rr$RR,pch=20)
 Nmuestro = as.integer(2*max(dato.rr$t))
-RRlineal = approx(dato.rr$t,dato.rr$RR,n=as.integer(71377.58399999907/0.25))
-RRspline = spline(dato.rr$t,dato.rr$RR,n=as.integer(71377.58399999907/0.25))
+RRlineal = approx(dato.rr$t,dato.rr$RR,n=delta_t)
+RRspline = spline(dato.rr$t,dato.rr$RR,n=delta_t)
 lines (RRlineal$x, RRlineal$y, col='red')
 points(RRlineal$x, RRlineal$y,pch=20,col='red')
 lines (RRspline$x,RRspline$y, col='blue')
@@ -30,12 +32,14 @@ points(dato.rr $t,dato.rr $RR,pch=20)
 
 
 #parte 2
-
+tiempo_total = 68927.72799999903
+constante_min_heartbeat = 0.25
+delta_t = as.integer(tiempo_total/constante_min_heartbeat)
 plot(dato.rr$t,dato.rr$RR,type='l', ylab='RR (ms)',xlab='tiempo(s)', xlim=c(0,72473),ylim=c(0,3000))
 points(dato.rr$t,dato.rr$RR,pch=20)
 Nmuestro = as.integer(2*max(dato.rr$t))
-RRlineal = approx(dato.rr$t,dato.rr$RR,n=as.integer(68927.72799999903/0.25))
-RRspline = spline(dato.rr$t,dato.rr$RR,n=as.integer(68927.72799999903/0.25))
+RRlineal = approx(dato.rr$t,dato.rr$RR,n=delta_t)
+RRspline = spline(dato.rr$t,dato.rr$RR,n=delta_t)
 lines (RRlineal$x, RRlineal$y, col='red')
 points(RRlineal$x, RRlineal$y,pch=20,col='red')
 lines (RRspline$x,RRspline$y, col='blue')
